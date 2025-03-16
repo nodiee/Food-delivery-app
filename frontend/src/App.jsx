0578b61af66faf9b93f44f7e0002c176c2e3f3ce
Priyam Vyas <priyamvyas005@gmail.com>
2025-03-17 01:54:28 +0530

    Fix Navbar import issue and optimize App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,34 @@
-import React, { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
-import Cart from './pages/Cart/Cart'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-import Home from './pages/Home/Home'
-import Footer from './components/Footer/Footer'
-import LoginPopup from './components/LoginPopup/LoginPopup'
-import Verify from './pages/verify/Verify'
-import MyOrders from './pages/MyOrders/MyOrders'
-import Navbar from './components/Navbar/Navbar';
-
-
-
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
+import Verify from "./pages/verify/Verify";
+import MyOrders from "./pages/MyOrders/MyOrders";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
-const [showLogin,setShowLogin]=useState(false)
+  const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
-    <div className='app'>
-     <Navbar setShowLogin={setShowLogin}/>
-     <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/Cart' element={<Cart/>}></Route>
-      <Route path='/order' element={<PlaceOrder/>}></Route>
-      <Route path='/verify' element={<Verify/>}></Route>
-      <Route path='/myorders' element={<MyOrders/>}></Route>
-
-     </Routes>
-    </div>
-    <Footer></Footer>
+      {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
+      <div className="app">
+        <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+          <Route path="/verify" element={<Verify />} />
+          <Route path="/myorders" element={<MyOrders />} />
+        </Routes>
+      </div>
+      <Footer />
     </>
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;
