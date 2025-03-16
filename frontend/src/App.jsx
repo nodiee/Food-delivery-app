e085816dd0002f38ffaee528efa209531c98a481
Priyam Vyas <priyamvyas005@gmail.com>
2025-03-17 01:49:29 +0530

    Fixed import path issue in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
+
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Home from './pages/Home/Home'
@@ -8,6 +8,7 @@ import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
 import Verify from './pages/verify/Verify'
 import MyOrders from './pages/MyOrders/MyOrders'
+import Navbar from './components/Navbar/Navbar';
 
 
 
